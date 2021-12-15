69e1bedf001f5286119e5dbb0658b5976d36ebe1
Abdul Razaq <arazaq917@gmail.com>
2021-12-15 18:36:26 +0500

    Today's work

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,27 +2,68 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: '',
-      confirmedName: ''
+      name: "",
+      lastName:'',
+      // fullName: "",
     };
   },
+  watch: {
+    counter(value){
+      if(value>50){
+        const that = this;
+        setTimeout(function(){
+          that.counter = 0;
+        },2000);
+      }
+    }
+  //   name(value) {
+  //     if (value === "") {
+  //       return (this.fullName = "");
+  //     } else {
+  //       this.fullName = value + " " + this.lastName;
+  //     }
+  //   },
+  //   lastName(value){
+  //     if (value === "") {
+  //       return (this.fullName = "");
+  //     } else {
+  //       this.fullName = this.name + " " + value;
+  //     }
+  //   }
+  },
+  computed: {
+    fullName() {
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      }
+      return this.name + " " + this.lastName;
+    },
+  },
   methods: {
-    confirmInput(){
-      this.confirmedName = this.name;
+    submitForm() {
+      alert("Form Submitted");
     },
-    submitForm(){
-      alert('Form Submitted');
-    },
-    add(num){
+    add(num) {
       this.counter = this.counter + num;
     },
-    remove(num){
+    remove(num) {
       this.counter = this.counter - num;
     },
-    setName(event){
-      this.name = event.target.value
-    }
+    // setFullName() {
+    //   console.log("I am Runing....");
+    //   if (this.name === "") {
+    //     return "";
+    //   }
+    //   return this.name + " " + "Suleman";
+    // },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    resetName() {
+      this.name = "";
+      this.lastName = "";
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
